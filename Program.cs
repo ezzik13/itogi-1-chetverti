b77cb381b072772526b6293ca94e66374fdb7efd
Nikolai <ezzik13@mail.ru>
2022-11-08 23:50:35 +0500

    программа готова

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры массив строк. Создать массив, со строками пользователя с количеством символов 3 или менее.
+using static System.Console;
+Clear();
+string[] strings = new string[4] { "1234", "2", "world", "computer science" };
+
+int count = 0;
+
+for (int i = 0; i < strings.Length; i++)
+{
+
+    if (strings[i].Length <= 3)
+    {
+        count++;
+    }
+
+}
+
+string[] result=new string[count];
+int j=0;
+for (int i = 0; i < strings.Length; i++)
+{
+    
+
+    if (strings[i].Length <= 3)
+    {
+        result[j]=strings[i];
+        j++;
+    }
+
+}
+WriteLine($"[{String.Join(",", result)}]");
